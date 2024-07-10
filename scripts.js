a7584e8d82fcd4b97d9ca398c26db31f3bda6014
punkhop <rockflip@gmail.com>
2024-07-10 13:27:51 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,9 +3,9 @@ const items = document.querySelectorAll('.carousel-item');
 const itemCount = items.length;
 
 function showNextItem() {
-    items[currentIndex].classList.remove('active');
-    currentIndex = (currentIndex + 1) % itemCount;
-    items[currentIndex].classList.add('active');
+	items[currentIndex].classList.remove('active');
+	currentIndex = (currentIndex + 1) % itemCount;
+	items[currentIndex].classList.add('active');
 }
 
 setInterval(showNextItem, 2000);
