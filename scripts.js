b6f77073c77e791982133310ed00c6049d665fab
punkhop <rockflip@gmail.com>
2024-07-10 23:06:12 -0700

    stars and sizing

--- scripts.js
+++ scripts.js
@@ -1,35 +1,10 @@
-// document.addEventListener('DOMContentLoaded', function() {
-//     var carousel = document.querySelector('.carousel-inner');
-//     var items = document.querySelectorAll('.carousel-item');
-//     var itemCount = items.length;
-//     let currentIndex = 0;
-
-//     function showNextItem() {
-//         currentIndex = (currentIndex + 1) % itemCount;
-//         updateCarousel();
-//     }
-
-//     function updateCarousel() {
-//         var offset = -currentIndex * 100;
-//         carousel.style.transform = `translateX(${offset}%)`;
-//     }
-
-//     // Initialize the carousel
-//     updateCarousel();
-
-//     // Change image every 5 seconds
-//     setInterval(showNextItem, 5000);
-
-//     console.log('Carousel initialized with ' + itemCount + ' items');
-// });
-
 // Stripe footer code
 
 document.addEventListener('DOMContentLoaded', function() {
-setTimeout(() => {
-    document.getElementById('holderImg').style.display = "none";
-    document.querySelector('.carousel-inner').style.display = "flex";
-}, 100);
+    setTimeout(() => {
+        document.getElementById('holderImg').style.display = "none";
+        document.querySelector('.carousel-inner').style.display = "flex";
+    }, 100);
 });
 
 if (document.getElementById('jetbookiframe')) {
